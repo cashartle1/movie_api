d120b6ddecef1d2514b81e506b280b3da3d1b0b3
cashartle1 <139818745+cashartle1@users.noreply.github.com>
2023-09-11 13:43:21 -0500

    initial creation with express & morgan
    adds express and morgan to index.js, adds top movies list array, and adds logger & error handling

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express'),
+    morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path');
+
+const app = express();
+
+//Top Movies List
+const topMoviesList = [
+    {
+        title: "movie 1",
+        director: "director 1",
+    }, 
+    {
+        title: "movie 2",
+        director: "director 2",
+    },
+    {
+        title: "movie 3",
+        director: "director 3",
+    },
+    {
+        title: "movie 4",
+        director: "director 4",
+    },
+    {
+        title: "movie 5",
+        director: "director 5",
+    },
+    {
+        title: "movie 6",
+        director: "director 6",
+    },
+    {
+        title: "movie 7",
+        director: "director 7",
+    },
+    {
+        title: "movie 8",
+        director: "director 8",
+    },
+    {
+        title: "movie 9",
+        director: "director 9",
+    },
+    {
+        title: "movie 10",
+        director: "director 10",
+    },
+];
+
+
+//create a write stream (in append mode)
+//a 'log.txt' file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my app!');
+});
+
+app.get("/movies", (req, res) => {
+    res.json(topMoviesList);
+});
+
+//requests for static files to the 'public' folder
+app.use(express.static('public'));
+
+//setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
+
+//error-handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
